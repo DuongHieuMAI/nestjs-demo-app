8d055a3e0f67b994b9c7f58e91f21de1703ba4ce
mai.duong.hieu-at-592964380541 <hieu.md@terra-drone.co.jp>
2019-07-02 11:45:51 +0700

    add simple document api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('User Service example')
+    .setDescription('The news API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .setBasePath('/api/v1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api/document', app, document);
+
   app.setGlobalPrefix('/api/v1');
   await app.listen(3000);
 }
